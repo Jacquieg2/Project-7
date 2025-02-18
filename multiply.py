517e1541c3812a4076b22e4cf207382907b87a8c
Jacquieg2 <garcija4@oregonstate.edu>
2025-02-18 18:49:19 -0500

    Create multiply.py

---
+++ multiply.py
@@ -0,0 +1,9 @@
+def multiply(a, b):
+    # Base case: if b is 1, the result is just a (since a * 1 = a)
+    if b == 1:
+        return a
+    # Recursive case: add a to the result of multiply(a, b - 1)
+    return a + multiply(a, b - 1)
+
+# Test the function
+print(multiply(7, 4))  # Output should be 28
